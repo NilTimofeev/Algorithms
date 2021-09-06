8887bfafbbb8b37447d2e859175dc030d6831b7d
Nil Timofeev <timofeevnil@mail.ru>
2021-09-06 13:33:53 +0300

    5

--- lesson_2/less4_1.py
+++ lesson_2/less4_1.py
@@ -1,26 +1,15 @@
 # 8. Посчитать, сколько раз встречается определенная цифра в введенной последовательности чисел.
 # Количество вводимых чисел и цифра, которую необходимо посчитать, задаются вводом с клавиатуры.
-
-
-def count_num_2():
-    """
-    python -m timeit -n 1000 -s "import less4_1" "less4_1.count_num_2()"
-    1000 loops, best of 5: 1.21 usec per loop
-    """
-    el = '3'
-    text = '233 67 8 330 13'
-    text = ''.join(text.split())
-    count_el = 0
-    for i in range(len(text)):
-        if text[i] == el:
-            count_el += 1
+"""
+В первом варианте работаем с числами и математикой, во втором со строкой.
+Ибираются строки ввода значений. Вместо них список чисел и строка.
+"""
 
 
 def count_num_1():
     """
     python -m timeit -n 1000 -s "import less4_1" "less4_1.count_num_1()"
-    1000 loops, best of 5: 2.05 usec per loop
-
+    1000 loops, best of 5: 29.8 nsec per loop
     """
     el = 3
     num_list = [233, 67, 8, 330, 13]
@@ -32,6 +21,22 @@ def count_num_1():
             num_list[i] //= 10
 
 
+def count_num_2():
+    """
+    python -m timeit -n 1000 -s "import less4_1" "less4_1.count_num_2()"
+    1000 loops, best of 5: 29.6 nsec per loop
+    """
+    el = '3'
+    text = '233 67 8 330 13'
+    text = ''.join(text.split())
+    count_el = 0
+    for i in range(len(text)):
+        if text[i] == el:
+            count_el += 1
+
+
 """
-Вариант со строкой гораздо быстрее математических вычислений
+Скорость выполнения одинаковая.
+Введенное значение будет строковым. Можно не делать преобразование типа и работать со строкой.
+Второй вариант выглядит интереснее.
 """
