88cc5ffc5ccfe4eb22e2e7a705a809728658574a
Nil Timofeev <timofeevnil@mail.ru>
2021-09-09 12:22:05 +0300

    готово

--- lesson_5/less5_task1.py
+++ lesson_5/less5_task1.py
@@ -1,22 +1,39 @@
 from collections import namedtuple
-num_comp = 2
-res = []
-Company = namedtuple('Company', 'name quarter_1 quarter_2 quarter_3 quarter_4')
+
+# Есть объекты со свойствами, которые задаются и не меняются. Поэтому для решения задачи выбрал именованный кортеж.
+
+num_comp = int(input('сколько компаний?:  '))
+comp_list = []
+Company = namedtuple('Company', 'name q1 q2 q3 q4 profit')
 sum_profit = 0
 
 for i in range(num_comp):
     name = input('name: ')
-    quarter_1 = input('profit_quarter_1:  ')
-    quarter_2 = input('profit_quarter_2:  ')
-    quarter_3 = input('profit_quarter_3:  ')
-    quarter_4 = input('profit_quarter_4:  ')
-    company = Company('name', quarter_1, quarter_2, quarter_3, quarter_4)
-    res.append(company)
+    quarter_1 = float(input('profit_quarter_1:  '))
+    quarter_2 = float(input('profit_quarter_2:  '))
+    quarter_3 = float(input('profit_quarter_3:  '))
+    quarter_4 = float(input('profit_quarter_4:  '))
 
+    profit = quarter_1 + quarter_2 + quarter_3 + quarter_4
     sum_profit += quarter_1 + quarter_2 + quarter_3 + quarter_4
-    print(company)
+    company = Company(name, quarter_1, quarter_2, quarter_3, quarter_4, profit)
+    comp_list.append(company)
 
 average = sum_profit / num_comp
+print(f'Средняя прибыль:  {average:.3f}', '\n')
+more_av = []
+less_av = []
 
-for i in range(num_comp):
-    comp_profit = res[i].quarter_1 + res[i].quarter_2 + res[i].quarter_3 + res[i].quarter_4
+for company in comp_list:
+    if company.profit < average:
+        less_av.append(company)
+    else:
+        more_av.append(company)
+
+print('Их прибыль ВЫШЕ среднеей:')
+for company in more_av:
+    print(f'{company.name} - {company.profit}')
+
+print('Их прибыль НИЖЕ среднеей:')
+for company in less_av:
+    print(f'{company.name} - {company.profit}')
