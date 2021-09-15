cd2b6ccf55776619dc68bd325050ee59170d552d
NilTimofeev <timofeevnil@mail.ru>
2021-09-15 20:18:02 +0300

    2 and 3

--- lesson_7/less1_task2.py
+++ lesson_7/less1_task2.py
@@ -3,26 +3,8 @@
 
 from random import  randint
 # функция слияния двух отсортированных списков
-def merge_list(a, b):
-    c = []
-    N = len(a)
-    M = len(b)
-
-    i = 0
-    j = 0
-    while i < N and j < M:
-        if a[i] <= b[j]:
-            c.append(a[i])
-            i += 1
-        else:
-            c.append(b[j])
-            j += 1
-
-    c += a[i:] + b[j:]
-    return c
 
 
-# функция деления списка и слияния списков в общий отсортированный список
 def split_and_merge_list(arr):
     middle = len(arr) // 2
     left = arr[:middle]     # деление массива на два примерно равной длины
@@ -33,10 +15,25 @@ def split_and_merge_list(arr):
     if len(right) > 1: # если длина 2-го списка больше 1, то делим дальше
         right = split_and_merge_list(right)
 
-    return merge_list(left, right)   # слияние двух отсортированных списков в один
+    result = []
+
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:] + right[j:]
+
+    return result
 
 
 array = [randint(0, 50) for i in range(10)]
-array = split_and_merge_list(array)
+print(array)
 
+array = split_and_merge_list(array)
 print(array)
---
+++ lesson_7/less7_task3.py
@@ -0,0 +1,42 @@
+# 3. Массив размером 2m + 1, где m — натуральное число, заполнен случайным образом. Найдите в массиве медиану.
+# Медианой называется элемент ряда, делящий его на две равные части: в одной находятся элементы, которые не меньше
+# медианы, в другой — не больше медианы.
+# Примечание: задачу можно решить без сортировки исходного массива. Но если это слишком сложно, используйте метод
+# сортировки, который не рассматривался на уроках (сортировка слиянием также недопустима).
+
+import random
+
+
+def quickselect_median(l, pivot_fn):
+    return quickselect(l, len(l) // 2, pivot_fn)
+
+
+def quickselect(l, k, pivot_fn):
+    """
+    Выбираем k-тый элемент в списке l (с нулевой базой)
+    :param l: список числовых данных
+    :param k: индекс
+    :param pivot_fn: функция выбора pivot, по умолчанию выбирает случайно
+    :return: k-тый элемент l
+    """
+
+    pivot = pivot_fn(l)
+
+    lows = [el for el in l if el < pivot]
+    highs = [el for el in l if el > pivot]
+    pivots = [el for el in l if el == pivot]
+
+    if k < len(lows):
+        return quickselect(lows, k, pivot_fn)
+    elif k < len(lows) + len(pivots):
+        # Нам повезло и мы угадали медиану
+        return pivots[0]
+    else:
+        return quickselect(highs, k - len(lows) - len(pivots), pivot_fn)
+
+
+m = 5
+array = [random.randint(0, 100) for _ in range(m * 2 + 1)]
+print(array)
+print(quickselect_median(array, random.choice))
+
