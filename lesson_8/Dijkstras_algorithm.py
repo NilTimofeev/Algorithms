5fe45ebe25377c1a62c4e8d035a3b45055534985
NilTimofeev <timofeevnil@mail.ru>
2021-09-19 19:39:37 +0300

    1, 2

--- lesson_8/Dijkstras_algorithm.py
+++ lesson_8/Dijkstras_algorithm.py
@@ -1,10 +1,13 @@
+# 2. Доработать алгоритм Дейкстры (рассматривался на уроке), чтобы он дополнительно возвращал список вершин,
+# которые необходимо обойти.
+
 gr = [
     [0, 0, 1, 1, 9, 0, 0, 0],
-    [1, 0, 9, 4, 0, 0, 5, 0],
-    [1, 9, 0, 0, 3, 0, 6, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 9, 4, 0, 0, 5, 0],
+    [0, 9, 0, 0, 3, 0, 6, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 5, 1],
+    [0, 0, 0, 0, 0, 0, 5, 0],
     [0, 0, 7, 0, 8, 1, 0, 0],
     [0, 0, 0, 0, 0, 1, 2, 0]
 ]
@@ -13,28 +16,38 @@ gr = [
 def dijkstra(graph, start):
     length = len(graph)
     is_visited = [False] * length
-    cost = [float('inf')] * length
-    parent = [-1] * length
+    cost = [float('inf')] * length  # стоимость пути до вершины. изначально бесконечность
+    parent = [-1] * length  # потом -1 поменяем на родителя
 
     cost[start] = 0
-    min_cost = 0
+    min_cost = 0  # будет показывать движемся по графу или уже нет
 
     while min_cost < float('inf'):
-        is_visited[start] = True
+        is_visited[start] = True  # в данный момент стартовая вершина, потом поменяем на другую
 
-        for i, vertex in enumerate(graph[start]):
-            if vertex != 0 and not is_visited[i]:
+        for i, vertex in enumerate(graph[start]):  # гоняем по строке графа соответсвующей стартовой вершине
+            if vertex != 0 and not is_visited[i]:  # значение вершины != 0, значит есть ребро и вершину не посещали
                 if cost[i] > vertex + cost[start]:
                     cost[i] = vertex + cost[start]
                     parent[i] = start
 
         min_cost = float('inf')
-        for i in range(length):
+        for i in range(length):  # цикл по всем вершинам графа
             if min_cost > cost[i] and not is_visited[i]:
                 min_cost = cost[i]
                 start = i
 
-    return cost
+    ways = []
+    for i in range(length):
+        j = i
+        way = [i]
+        while parent[j] != -1:
+            way.append(parent[j])
+            j = parent[j]
+        ways.append(way)
+    return cost, ways
+
+    return cost, ways
 
 
 s = 0
--- lesson_8/lesson.py
+++ lesson_8/lesson.py
@@ -1,3 +1,11 @@
+# последовательный просмотр отдельных уровней графа, начиная с узла источника
+# берем первую вершину и помещаем в пустую очередь.
+# далее цикл. извлекаем из очереди вершину. если вершина целевая, то конец поиска.
+# тначе в конец очереди добавляются все смежные вершины, которые еще не пройдены и не находятся
+# в очереди.
+# далее следующий виток цикла. если очередь пуста, то все вершины графа просмотрены и целевой узел не достижим
+
+
 from collections import deque
 
 gr = [
@@ -14,32 +22,32 @@ print(*gr, sep='\n')
 
 
 def bfs(graph, start, finish):
-    parent = [None for _ in range(len(graph))]
+    parent = [None for _ in range(len(graph))]  # родитель для каждой вершины
     is_visited = [False for _ in range(len(graph))]
 
     deq = deque([start])
-    is_visited[start] = True
+    is_visited[start] = True  # посетили 1 вершину, не будем заходить в нее в цикле
 
     while len(deq) > 0:
-        curent = deq.pop()
+        curent = deq.pop()  # извлекаем вершину, отправная точка
         if curent == finish:
             # return parent
             break
-
-        for i, vertex in enumerate(graph[curent]):
-            if vertex == 1 and not is_visited[i]:
+        # смотри мвсе вершины, связанные с текущей
+        for i, vertex in enumerate(graph[curent]):  # строка curent из графа
+            if vertex == 1 and not is_visited[i]:  # ЕСЛИ 1, значит можем перейти в вершину из текущей и не посещали еще
                 is_visited[i] = True
-                parent[i] = curent
+                parent[i] = curent  # указываем в списке откуда пришли
                 deq.appendleft(i)
 
     else:
         return f'из {start} не попасть в {finish}'
 
     cost = 0
-    way = deque([finish])
-    i = finish
+    way = deque([finish])  # добавим в очередь цель
+    i = finish  # потребуется, чтобы пройтись по вершщинам от конца к началу
 
-    while parent[i] != start:
+    while parent[i] != start:  # пока не доши до первой вершины
         cost += 1
         way.appendleft(parent[i])
         i = parent[i]
@@ -47,7 +55,7 @@ def bfs(graph, start, finish):
     cost += 1
     way.appendleft(start)
 
-    return f'путь {way} длинной {cost}'
+    return f'путь {list(way)} длинной {cost}'
 
 
 s = 1
