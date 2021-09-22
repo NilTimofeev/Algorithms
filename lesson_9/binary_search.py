6a665a2963146a141c9f2d3503424f6a0dbe9b96
Nil Timofeev <timofeevnil@mail.ru>
2021-09-22 13:48:38 +0300

    less8

---
+++ lesson_9/binary_search.py
@@ -0,0 +1,23 @@
+from binarytree import bst
+
+
+def search(bin_search_tree, number, path=''):
+
+    if bin_search_tree.value == number:
+        return f'Число {number} найдено по пути: \n{path}'
+
+    if number < bin_search_tree.value and bin_search_tree.left != None:
+        return search(bin_search_tree.left, number, path=f'{path}\nШаг влево')
+
+    if number > bin_search_tree.value and bin_search_tree.right != None:
+        return search(bin_search_tree.right, number, path=f'{path}\nШаг вправо')
+
+    return f'Число отсутствует'
+
+
+bt = bst(height=5, is_perfect=False)
+print(bt)
+
+num = int(input('введите число:  '))
+print(search(bt, num))
+
